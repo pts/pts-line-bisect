b8205f815bff49b18e5339bd7a304e40ea9b7575
Peter Szabo <pts@fazekas.hu>
2013-11-30 06:23:47 +0100

    added bisect_way

--- pts_lbsearch.c
+++ pts_lbsearch.c
@@ -55,7 +55,7 @@ void yfopen(yfile *yf, const char *pathname, off_t size) {
   }
   if (size == -1) {
     size = lseek(fd, 0, SEEK_END);
-    if (size + 1U == 0U) {
+    if (size + 1ULL == 0ULL) {
       fprintf(stderr, "error: lseek end: %s\n", strerror(errno));
       exit(2);
     }
@@ -126,7 +126,7 @@ int yfgetc(yfile *yf) {
     yf->p = a - b + yf->rbuf;
     if (yf->ofs != b) {
       a = lseek(yf->fd, b, SEEK_SET);
-      if (a + 1U == 0U) {
+      if (a + 1ULL == 0ULL) {
         fprintf(stderr, "error: lseek set: %s\n", strerror(errno));
         exit(2);
       }
@@ -200,6 +200,131 @@ off_t get_fofs(yfile *yf, off_t ofs) {
     ++ofs;
     if (c == '\n') return ofs;
   }
+}
+
+enum CacheEntryResult {
+  CER_CMP_FALSE = 0,
+  CER_CMP_TRUE = 1,
+  CER_UNUSED = 2,
+};
+
+struct cache_entry {
+  off_t ofs;
+  off_t fofs;
+  ybool cmp_result;
+};
+
+struct cache {
+  struct cache_entry e[2];
+  /* 0: 01 used, 0 is active;
+   * 1: 01 used, 1 is active;
+   * 2: 0 used, 0 is active;
+   * 3: none used.
+   */
+  int active;
+};
+
+#define CACHE_HAS_0(a) ((a) != 3)
+#define CACHE_HAS_1(a) ((a) < 2)
+#define CACHE_GET_ACTIVE(a) ((a) & 1)  /* Valid only if CACHE_HAS_0(a). */
+
+/** Can be called again to clear the cache. */
+void cache_init(struct cache *cache) {
+  cache->active = 3;
+}
+
+const struct cache_entry *get_using_cache(
+    yfile *yf, struct cache *cache, off_t ofs,
+    const char *x, size_t xsize, compare_mode_t cm) {
+  int a = cache->active;
+  struct cache_entry *entry;
+  off_t fofs;
+  assert(ofs >= 0);
+  if (CACHE_HAS_0(a) &&
+      cache->e[0].ofs <= ofs && ofs <= cache->e[0].fofs) {
+    if (a == 1) cache->active = a = 0;
+  } else if (CACHE_HAS_1(a) &&
+             cache->e[1].ofs <= ofs && ofs <= cache->e[1].fofs) {
+    if (a == 0) cache->active = a = 1;
+  } else {
+    fofs = get_fofs(yf, ofs);
+    assert(ofs <= fofs);
+    if (CACHE_HAS_0(a) && cache->e[0].fofs == fofs) {
+      if (a == 1) cache->active = a = 0;
+      if (cache->e[0].ofs > ofs) cache->e[0].ofs = ofs;
+    } else if (CACHE_HAS_1(a) && cache->e[1].fofs == fofs) {
+      if (a == 0) cache->active = a = 1;
+      if (cache->e[1].ofs > ofs) cache->e[1].ofs = ofs;
+    } else {
+      if (CACHE_HAS_0(a)) {
+        cache->active = a = CACHE_GET_ACTIVE(a) ^ 1;
+        entry = cache->e + a;
+      } else {
+        cache->active = a = 2;
+        entry = cache->e;
+      }
+      /* Fill newly activated cache entry. */
+      entry->fofs = fofs;
+      entry->ofs = ofs;
+      entry->cmp_result = compare_line(yf, fofs, x, xsize, cm);
+      return entry;  /* Shortcut, the return below would do the same. */
+    }
+  }
+  return cache->e + CACHE_GET_ACTIVE(a);
+}
+
+off_t get_fofs_using_cache(
+    yfile *yf, struct cache *cache, off_t ofs) {
+  int a = cache->active;
+  off_t fofs;
+  assert(ofs >= 0);
+  if (CACHE_HAS_0(a) &&
+      cache->e[0].ofs <= ofs && ofs <= cache->e[0].fofs) {
+    if (a == 1) cache->active = a = 0;
+    return cache->e[0].fofs;
+  } else if (CACHE_HAS_1(a) &&
+             cache->e[1].ofs <= ofs && ofs <= cache->e[1].fofs) {
+    if (a == 0) cache->active = a = 1;
+    return cache->e[1].fofs;
+  } else {
+    fofs = get_fofs(yf, ofs);
+    assert(ofs <= fofs);
+    if (CACHE_HAS_0(a) && cache->e[0].fofs == fofs) {
+      if (a == 1) cache->active = a = 0;
+      if (cache->e[0].ofs > ofs) cache->e[0].ofs = ofs;
+    } else if (CACHE_HAS_1(a) && cache->e[1].fofs == fofs) {
+      if (a == 0) cache->active = a = 1;
+      if (cache->e[1].ofs > ofs) cache->e[1].ofs = ofs;
+    }
+    /* We don't update the cache, because we don't know cmp_result, and we
+     * are too lazy to compute it.
+     */
+    return fofs;
+  }
+}
+
+off_t bisect_way(
+    yfile *yf, struct cache *cache, off_t lo, off_t hi,
+    const char *x, size_t xsize, compare_mode_t cm) {
+  const off_t size = yfgetsize(yf);
+  off_t mid, midf;
+  const struct cache_entry *entry;
+  if (hi + 0ULL > size + 0ULL) hi = size;  /* Allso applies to hi == -1. */
+  while (xsize > 0 && x[xsize - 1] == '\n') --xsize;
+  /* is_left=true corresponds to cm=CM_LE */
+  if (cm == CM_LE && xsize == 0) return 0;  /* Shortcut. */
+  if (lo >= hi) return get_fofs_using_cache(yf, cache, lo);
+  do {
+    mid = (lo + hi) >> 1;
+    entry = get_using_cache(yf, cache, mid, x, xsize, cm);
+    midf = entry->fofs;
+    if (entry->cmp_result) {
+      hi = mid;
+    } else {
+      lo = mid + 1;
+    }
+  } while (lo < hi);
+  return mid == lo ? midf : get_fofs_using_cache(yf, cache, lo);
 }
 
 /* --- main */
